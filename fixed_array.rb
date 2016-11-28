c225ae47172d600f1e3a7dd05dbe72a702f8d547
Jillian Somera <jsomera@gmail.com>
2016-11-27 18:51:54 -0600

    Write and pass tests

--- fixed_array.rb
+++ fixed_array.rb
@@ -1,3 +1,25 @@
 class FixedArray
+# FixedArray#new(size): Instantiate a new FixedArray with space for size elements.
+
+	def initialize(size = 16)
+		@array = Array.new(size, nil)
+	end	
+
+	def size
+		@array.length
+	end
+# FixedArray#get(index): Get a value from the array at the specified index. Throw an OutOFBoundsException if the user tries to get a value at an index outside the bounds of the fixed array.
+	def get(index)
+		raise IndexOutOfBoundsError if index >= self.size || index < 0
+    @array[index]
+	end
+
+# FixedArray#set(index, element): Set a value in the array at a specific index and return the element. Throw an OutOFBoundsException if the user tries to set a value at an index outside the bounds of the fixed array.
+	def set(index, element)
+		raise IndexOutOfBoundsError if element[index] >= self.size || index < 0
+    @array[index]
+	end
 
 end
+
+class IndexOutOfBoundsError < StandardError; end
--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -2,4 +2,28 @@ require_relative 'fixed_array'
 
 describe FixedArray do
   
+	describe '#new' do
+		it 'has a fixed size by default' do
+			array = FixedArray.new
+			expect(array.size).to eq 16
+		end
+
+		it 'can create an array with the indicated size' do
+			size = 64
+			array = FixedArray.new(size)
+			expect(array.size).to eq size
+		end
+	end
+
+	describe '#get' do
+		it 'returns a value at a specific position' do
+			array = FixedArray.new
+			expect(array.get(1)).to eq nil
+		end
+		it 'raises an IndexOutOfBoundsError if a retrieved value is out of bounds in the array' do
+			array = FixedArray.new(4)
+			expect { array.get(6)}.to raise_error(IndexOutOfBoundsError) 
+		end
+	end
+
 end
